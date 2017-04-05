cf50dea52b5e4701deef04983fd985653864a6d6
Matt McDonald <mattmcd25@gmail.com>
2017-04-04 23:02:56 -0400

    Entering the DirectionsViewController now finds the path.

--- src/controllers/DirectionsViewController.java
+++ src/controllers/DirectionsViewController.java
@@ -8,18 +8,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import models.path.Node;
 
 import java.io.IOException;
 
 /**
  * Created by mattm on 3/29/2017.
  */
-public class DirectionsViewController {
+public class DirectionsViewController implements IControllerWithParams {
+    private Node startNode;
+    private Node endNode;
+
     @FXML
     private Button backBtn;
 
     @FXML
     private void clickBack(ActionEvent event) {
-        KioskMain.setScene("views/MainMenu.fxml");
+        KioskMain.setScene("views/FinalDestSelectionView.fxml", this.startNode);
+    }
+
+    @Override
+    public void initData(Object... data) {
+        this.startNode = (Node)data[0];
+        this.endNode = (Node)data[1];
+
+        System.out.println(KioskMain.getPath().findPath(this.startNode, this.endNode));
     }
 }
