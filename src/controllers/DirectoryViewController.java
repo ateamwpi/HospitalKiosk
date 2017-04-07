557d51b0ab71407c19e2e17e707e71fd7921b95f
Kevin O'Brien <kjrobrien@gmail.com>
2017-04-07 18:30:24 -0400

    Create abstract class for directory controller

--- src/controllers/DirectoryViewController.java
+++ src/controllers/DirectoryViewController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import models.dir.Directory;
 import models.dir.Location;
 import models.dir.LocationType;
@@ -18,45 +19,26 @@ import java.util.*;
  * Created by mattm on 3/29/2017.
  */
 
-public class DirectoryViewController {
-
-    private HashMap<LocationType, Directory> directories;
-
-    private Collection<Location> selectedLocations; // all Locations of the current LocationType
-
-    private Collection<Location> filteredLocations; // all Locations that match the searchBox
+public class DirectoryViewController extends DirectoryController {
 
     private Node startNode; // The selected starting node for pathfinding
 
     private Node endNode;
 
-    private Node selectedNode;
-
-
+    @FXML
+    private AnchorPane directoryView;
     @FXML
     private Label title;
     @FXML
     private Button backBtn;
     @FXML
-    private TableView<Location> locationsTable; //table to hold all locations
-    @FXML
-    private TableColumn<Location, String> nameCol; //column that holds names of locations
-    @FXML
-    private TableColumn<Location, String> roomCol; //column that holds room names
-    @FXML
     private Button goToFinalSel; //button user will press to get path to selected
     @FXML
     private Label directions; //label to give user instructions
-    @FXML
-    private TextField searchBox;
-    @FXML
-    private ComboBox<String> locationDropdown;
 
 
     public DirectoryViewController() {
-        directories = KioskMain.getDir().getDirectories();
-        selectedLocations = new ArrayList<>();
-        filteredLocations = new ArrayList<>();
+        super();
     }
 
     @FXML
@@ -67,46 +49,14 @@ public class DirectoryViewController {
         goToFinalSel.setDisable(true);
 
         initializeTable();
-        initializeDropdown();
-        initializeFilter();
-    }
-    /**
-     * Populate the Table of Locations. Sorts the table by name. When entry is selected, allow for use as starting
-     * location.
-     */
-    private void initializeTable() {
-        nameCol.setCellValueFactory(new PropertyValueFactory("name"));
-        roomCol.setCellValueFactory(new PropertyValueFactory("roomName"));
-        locationsTable.getSortOrder().add(nameCol); // Default to sort by name
         locationsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
             if (locationsTable.getSelectionModel().getSelectedItem() != null) {
-                this.selectedNode = newValue.getNode(); //store the node that was selected
+                this.selectedLocation = newValue;
                 goToFinalSel.setDisable(false); //enable -> button once selection has been made
             }
         });
-    }
-
-    /**
-     * Populate LocationType Dropdown menu with all LocationTypes. Additionally, allow for Full Directory to be selected.
-     */
-    private void initializeDropdown() {
-        locationDropdown.getSelectionModel().selectedItemProperty().addListener(((observable, oldValue, newValue) -> {
-            selectDirectory(locationDropdown.getSelectionModel().getSelectedItem());
-        }));
-        locationDropdown.getItems().add("Full Directory");
-        for (LocationType locType : LocationType.values()) {
-            locationDropdown.getItems().add(locType.name());
-        }
-        locationDropdown.getSelectionModel().selectFirst();
-    }
-
-    private void initializeFilter() {
-        searchBox.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                filterLocations();
-            }
-        });
+        initializeDropdown();
+        initializeFilter();
     }
 
     @FXML  //when user clicks "back" button, they will return to main menu
@@ -121,90 +71,11 @@ public class DirectoryViewController {
             directions.setText("Select an ending location from the table below. Once a location is selected, click the 'Get Path' button " +
                     "to view a path connecting the  selected starting and ending locations.");
             goToFinalSel.setText("Get Path");
-            startNode = selectedNode;
+            startNode = selectedLocation.getNode();
         } else {
-            endNode = selectedNode;
+            endNode = selectedLocation.getNode();
             KioskMain.setScene("views/DirectionsView.fxml", this.startNode, this.endNode);
         }
-    }
-
-    private void filterLocations() {
-        filteredLocations.clear(); // Clear the list keeping track of filtered locations
-        String filterString = searchBox.getText().toLowerCase(); // get the keyword
-        // check which locations contain the keyword
-        for (Location loc : selectedLocations) {
-            if (loc.getName().toLowerCase().contains(filterString)) {
-                filteredLocations.add(loc);
-            }
-        }
-        showLocations(filteredLocations); // update the table to reflect the filteredLocations
-    }
-
-    /**
-     * Establishes all locations to search through, displays them on table
-     * @param locations
-     */
-    private void selectLocations(Collection<Location> locations) {
-        searchBox.clear();
-        filteredLocations.clear();
-        selectedLocations = locations;
-        showLocations(locations);
-    }
-
-    /**
-     * Shows the given locations on the table and sorts them.
-     * @param locations
-     */
-    private void showLocations(Collection<Location> locations) {
-        locationsTable.getItems().setAll(locations);
-        locationsTable.sort();
-    }
-
-    /**
-     * Select the LocationType to use for the directory.
-     * @param locType
-     */
-    private void selectDirectory(LocationType locType) {
-        selectLocations(getLocationsOfType(locType));
-    }
-
-    /**
-     * Selects the directory based upon a given string.
-     * @param s
-     */
-    private void selectDirectory(String s) {
-        if (s.equalsIgnoreCase("Full Directory")) {
-            setFullDirectory();
-        } else {
-            selectDirectory(LocationType.getType(s));
-        }
-    }
-
-    /**
-     * Selects the locations based upon every location type.
-     */
-    private void setFullDirectory() {
-        Collection<Location> locations = new ArrayList<Location>(); //make an array list of location// s
-        KioskMain.getDir().getDirectories().values();
-        for(LocationType locType : LocationType.values()) {
-            locations.addAll(getLocationsOfType(locType));
-        }
-        //set the list of locations shown on table to be all the locations added above
-        selectLocations(locations);
-    }
-
-    /**
-     * Find locations of a given type.
-     * @param locType
-     * @return Collection of Locations of a given LocationType.
-     */
-    private Collection<Location> getLocationsOfType(LocationType locType) {
-        if (directories.containsKey(locType)) {
-            Directory dir = directories.get(locType);
-            HashMap<Integer, Location> locations = dir.getLocations();
-            return locations.values();
-        }
-        return new ArrayList<Location>(); //returns empty array list if there are no locations of given type
     }
 
 }
