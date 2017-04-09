8e00dfcb15c5a4f3aa5a8128869232ff571b6194
Dylan Richardson <dylanrichardson1996@gmail.com>
2017-04-09 18:15:32 -0400

    Move ChangeKioskController to admin package

--- src/controllers/admin/AdminMenuController.java
+++ src/controllers/admin/AdminMenuController.java
@@ -1,19 +1,11 @@
 package controllers.admin;
 
 import controllers.AbstractController;
-import controllers.ChangeKioskController;
-import controllers.DirectoryViewController;
 import controllers.MainMenuController;
 import core.KioskMain;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * Created by mattm on 3/29/2017.
